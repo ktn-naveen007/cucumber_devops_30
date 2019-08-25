ffcd435dfad6914861932b6a23aa812f6ea08ea0
Navin Tirumal Kotamraju <navin.kotamraju@yash.com>
2019-08-25 14:27:38 +0530

    04

--- src/main/java/com/automation/cucumber/Runner.java
+++ src/main/java/com/automation/cucumber/Runner.java
@@ -9,16 +9,16 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.Listeners;
 
 import com.automation.cucumber.TestListener;
+ import com.vimalselvam.cucumber.listener.Reporter;
 
-import com.cucumber.listener.Reporter;
 
 
 
 
 @Listeners({ TestListener.class })
 
-@CucumberOptions(features = { "E:\\Cucumber_Naveen\\cucumber\\FeatureFiles" }, glue = {"E:\\Cucumber_Naveen\\cucumber\\src\\main\\java\\com\\cucumber\\steps"}, plugin = { "pretty","json:target/cucumber-reports/Cucumber.json",
-		"com.cucumber.listener.ExtentCucumberFormatter:Extent Reports/report.html" }, monochrome = true, strict = false, dryRun = false, tags = {
+@CucumberOptions(features = { "E:\\Cucumber_Naveen\\cucumber\\FeatureFiles" }, glue = "", plugin = { "pretty","json:target/cucumber-reports/Cucumber.json",
+		"com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:Extent Reports/report.html" }, monochrome = true, strict = false, dryRun = false, tags = {
 				"@Functional1" })
 
 public class Runner extends AbstractTestNGCucumberTests {
@@ -26,7 +26,7 @@ public class Runner extends AbstractTestNGCucumberTests {
 	@AfterClass
 	public static void writeExtentReport() {
 
-		Reporter.loadXMLConfig(new File("E:\\Cucumber_Naveen\\cucumber\\extent-config.xml"));
+	Reporter.loadXMLConfig(new File("E:\\Cucumber_Naveen\\cucumber\\extent-config.xml"));
 
 	}
 }
