6034255249cd6e27f7902031242466c00cf60c05
Navin Tirumal Kotamraju <navin.kotamraju@yash.com>
2019-08-30 20:26:50 +0530

    101

--- src/main/java/com/cucumber/pages/Base.java
+++ src/main/java/com/cucumber/pages/Base.java
@@ -9,7 +9,7 @@ public class Base {
 	int count=0;
 	public WebDriver instance(){
 		if(driver == null){
-			System.setProperty("webdriver.chrome.driver", "E:\\Girish-Cucumber\\BDD_using_Selenium_Cucumber\\BDD_using_Selenium_Cucumber\\Drivers\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "./Drivers//chromedriver.exe");
 			driver = new ChromeDriver();
 			System.out.println("count=="+ ++count);
 		}
